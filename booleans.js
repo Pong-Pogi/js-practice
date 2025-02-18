5826968641d7e211cbf535c16e4bab01f239a3c6
Pong-Pogi <pauldiano430@gmail.com>
2025-02-18 19:18:38 +0700

    Add files via upload

---
+++ booleans.js
@@ -0,0 +1,9 @@
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumb = 7;
+let undefinedValue = undefined;
+
+console.log(isJsProgrammingLanguage);
+console.log(isJsHard);
+console.log(favNumb);
+console.log(undefinedValue);
