9e3fc04d6c3161ae8ee28db283d5fba72a1e34d0
Pong-Pogi <pauldiano430@gmail.com>
2025-02-18 19:40:25 +0700

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,10 @@
+let favActorFirstName = "Roberto";
+let favActorLastName = "Downy ANTIBAC";
+let fullName = favActorFirstName + " " + favActorLastName;
+let uppercase = fullName.toUpperCase();
+let message = `My favorite actor is ${uppercase}.`;
+message += " His best show is Avengers: Doomsday ";
+
+console.log(fullName);
+console.log(uppercase);
+console.log(message);
