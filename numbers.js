b7aa421acdada3dabb2c8da0d986b3bee6e66ed9
Pong-Pogi <pauldiano430@gmail.com>
2025-02-18 19:10:05 +0700

    Add files via upload

---
+++ numbers.js
@@ -0,0 +1,9 @@
+let firstFavNum = 7;
+let secondFavNum = 3;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
