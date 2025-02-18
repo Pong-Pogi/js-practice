93618540ba4d8ae84799567b7714f860bb749ba9
Pong-Pogi <pauldiano430@gmail.com>
2025-02-18 19:25:31 +0700

    Add files via upload

---
+++ comparison.js
@@ -0,0 +1,11 @@
+let firstFavNumb = 5;
+let secondFavNum = 3;
+
+console.log(firstFavNumb > secondFavNum);
+console.log(firstFavNumb < secondFavNum);
+console.log(firstFavNumb >= secondFavNum);
+console.log(firstFavNumb <= secondFavNum);
+console.log(firstFavNumb === secondFavNum);
+console.log(firstFavNumb == secondFavNum);
+console.log(firstFavNumb !== secondFavNum);
+console.log(firstFavNumb != secondFavNum);
