c4f241f7ee88d2a9aa73fffb1dda31a573c43c4d
Pong-Pogi <pauldiano430@gmail.com>
2025-02-18 19:05:35 +0700

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,9 @@
+let name = "John Paul Diano";
+let whatDoYouWannaBecomeInYourLife = "Programmer";
+let gender = "Male";
+let twitterHandle = "@yourTwitterHandle";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
